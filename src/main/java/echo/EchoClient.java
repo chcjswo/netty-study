ea160ff0a0997ad60580f3cfee8ec32662538f8d
tony <chcjswo@gmail.com>
2019-08-04 16:25:51 +0900

    inner class로 수정

--- src/main/java/echo/EchoClient.java
+++ src/main/java/echo/EchoClient.java
@@ -1,17 +1,18 @@
 package echo;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
+import java.nio.charset.Charset;
+
 /**
  * Created by chcjswo on 2019-08-04
- *
+ * <p>
  * Blog: http://mocadev.tistory.com
  * Github: http://github.com/chcjswo
  */
@@ -25,7 +26,7 @@ public class EchoClient {
 					.channel(NioSocketChannel.class)
 					.handler(new ChannelInitializer<SocketChannel>() {
 						@Override
-						public void initChannel(SocketChannel ch) throws Exception {
+						public void initChannel(SocketChannel ch) {
 							ChannelPipeline p = ch.pipeline();
 							p.addLast(new EchoClientHandler());
 						}
@@ -38,6 +39,70 @@ public class EchoClient {
 			e.printStackTrace();
 		} finally {
 			group.shutdownGracefully();
+		}
+	}
+
+	public static class EchoClientHandler extends ChannelInboundHandlerAdapter {
+		@Override
+		public void channelActive(ChannelHandlerContext ctx) {
+			String sendMessage = "Hello, Netty!";
+
+			ByteBuf messageBuffer = Unpooled.buffer();
+			messageBuffer.writeBytes(sendMessage.getBytes());
+
+			StringBuilder builder = new StringBuilder();
+			builder.append("전송한 문자열: [").append(sendMessage).append("]");
+
+			System.out.println(builder.toString());
+
+			ctx.writeAndFlush(messageBuffer);
+		}
+
+		/**
+		 * Calls {@link ChannelHandlerContext#fireChannelRead(Object)} to forward
+		 * to the next {@link ChannelInboundHandler} in the {@link ChannelPipeline}.
+		 * <p>
+		 * Sub-classes may override this method to change behavior.
+		 *
+		 * @param ctx ChannelHandlerContext
+		 * @param msg Object
+		 */
+		@Override
+		public void channelRead(ChannelHandlerContext ctx, Object msg) {
+			String readMessage = ((ByteBuf) msg).toString(Charset.defaultCharset());
+
+			StringBuilder builder = new StringBuilder();
+			builder.append("수신한 문자열: [").append(readMessage).append("]");
+
+			System.out.println(builder.toString());
+		}
+
+		/**
+		 * Calls {@link ChannelHandlerContext#fireChannelReadComplete()} to forward
+		 * to the next {@link ChannelInboundHandler} in the {@link ChannelPipeline}.
+		 * <p>
+		 * Sub-classes may override this method to change behavior.
+		 *
+		 * @param ctx ChannelHandlerContext
+		 */
+		@Override
+		public void channelReadComplete(ChannelHandlerContext ctx) {
+			ctx.close();
+		}
+
+		/**
+		 * Calls {@link ChannelHandlerContext#fireExceptionCaught(Throwable)} to forward
+		 * to the next {@link ChannelHandler} in the {@link ChannelPipeline}.
+		 * <p>
+		 * Sub-classes may override this method to change behavior.
+		 *
+		 * @param ctx ChannelHandlerContext
+		 * @param cause Throwable
+		 */
+		@Override
+		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+			cause.printStackTrace();
+			ctx.close();
 		}
 	}
 }
--- src/main/java/echo/EchoServer.java
+++ src/main/java/echo/EchoServer.java
@@ -1,13 +1,13 @@
 package echo;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.nio.charset.Charset;
 
 /**
  * Created by chcjswo on 2019-08-03
@@ -16,30 +16,53 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
  * Github: http://github.com/chcjswo
  */
 public class EchoServer {
-    public static void main(String[] args) throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
+	public static void main(String[] args) {
+		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
 
-        try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) {
-                            ChannelPipeline p = ch.pipeline();
-                            p.addLast(new EchoServerHandler());
-                        }
-                    });
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				public void initChannel(SocketChannel ch) {
+					ChannelPipeline p = ch.pipeline();
+					p.addLast(new EchoServerHandler());
+				}
+			});
 
-            ChannelFuture f = b.bind(8888).sync();
+			ChannelFuture f = b.bind(8888).sync();
 
-            f.channel().closeFuture().sync();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
-        }
-    }
+			f.channel().closeFuture().sync();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+	}
+
+	public static class EchoServerHandler extends ChannelInboundHandlerAdapter {
+		@Override
+		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+			cause.printStackTrace();
+			ctx.close();
+		}
+
+		@Override
+		public void channelRead(ChannelHandlerContext ctx, Object msg) {
+			String readMessage = ((ByteBuf) msg).toString(Charset.defaultCharset());
+
+			System.out.println("수신한 문자열: [" + readMessage + "]");
+
+			ctx.write(msg);
+		}
+
+		@Override
+		public void channelReadComplete(ChannelHandlerContext ctx) {
+			ctx.flush();
+		}
+	}
 }
+
